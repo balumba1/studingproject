7cfe40fdfd79c935f97eab6a7bbeaba08cc1f962
Adrian Hunka <adik50010@gmail.com>
2023-08-03 13:14:29 +0300

    coment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const numberOfFilms = +prompt("How much films you already watched?", "");
 const personalMovieDB = {
   count: numberOfFilms,
@@ -14,3 +15,35 @@ const a = prompt("Last film you watched?", ""),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+
+if (4 == 9) {
+  console.log("OK");
+} else {
+  console.log("Error");
+}
+
+// if (num < 49) {
+//   console.log("Error");
+// } else if (num > 100) {
+//   console.log("too much");
+// } else {
+//   console.log("Ok");
+// }
+
+// num === 50 ? console.log("Ok") : console.log("Error");
+
+const num = 51;
+switch (num) {
+  case 49:
+    console.log("Wrong");
+    break;
+  case 100:
+    console.log("Wrong");
+    break;
+  case 50:
+    console.log("Nice");
+    break;
+  default:
+    console.log("Not today");
+    break;
+}
