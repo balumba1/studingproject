c1388324598cf85c5cbf409079c9805ed832d235
Adrian Hunka <adik50010@gmail.com>
2023-08-10 17:27:08 +0300

    praktika6

--- js/script.js
+++ js/script.js
@@ -1,44 +1,78 @@
 "use strict";
-// const numberOfFilms = +prompt("How much films you already watched?", "");
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
-// const a = prompt("Last film you watched?", ""),
-//   b = prompt("How good is was?", ""),
-//   c = prompt("Last film you watched?", ""),
-//   d = prompt("How good is was?", "");
+let numberOfFilms;
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+function start() {
+  numberOfFilms = +prompt("How much films you already watched?", "");
 
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt("Last film you watched?", ""),
-//     b = prompt("How good is was?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("How much films you already watched?", "");
+  }
+}
 
-//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   } else {
-//     console.log("error");
-//     i--;
-//   }
-// }
+start();
 
-// if (personalMovieDB.count < 10) {
-//   console.log("Mala");
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//   console.log("Clasic");
-// } else if (personalMovieDB.count >= 30) {
-//   console.log("Kinoman");
-// } else {
-//   console.log("Error");
-// }
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Last film you watched?", ""),
+  b = prompt("How good is was?", ""),
+  c = prompt("Last film you watched?", ""),
+  d = prompt("How good is was?", "");
 
-// console.log(personalMovieDB);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Last film you watched?", ""),
+      b = prompt("How good is was?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Mala");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Clasic");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Kinoman");
+  } else {
+    console.log("Error");
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; (i) => 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(
+      `Your favorite genre by number ${i}`
+    );
+  }
+}
+writeYourGenres();
 
 // if (4 == 9) {
 //   console.log("OK");
@@ -175,3 +209,87 @@
 // }
 
 // console.log(result)
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//   console.log(text);
+//   let num = 10;
+//   console.log(num);
+// }
+
+// showFirstMessage("Hello World");
+// console.log(num);
+
+// function calc(a, b) {
+//   return a + b;
+// }
+
+// console.log(calc(4, 3));
+
+// function ret() {
+//   let num = 50;
+//   return num;
+// }
+// const anothernum = ret();
+// console.log(anothernum);
+// const logger = function () {
+//   console.log("Hello");
+// };
+// logger();
+
+// const calc = (a, b) => {
+//   return a + b;
+// };
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//   return curr * amount;
+// }
+
+// function promotion(result) {
+//   console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// function test() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     if (i === 3) return;
+//   }
+//   console.log("Done");
+// }
+// test();
+
+// function doNothing() {}
+// console.log(doNothing() === undefined);
+
+// const str = 'test';
+
+// console.log(str[2]);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = 'Some fruit';
+//  console.log(fruit.indexOf("q"));
+
+//  const logg = "Hello world";
+
+//  console.log(logg.slice(6));
+
+//  console.log(logg.substring(6, 11));
+
+//  console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px"
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
