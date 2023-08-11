66b32811e2074ff781a2e01c639f373fbda0c682
Adrian Hunka <adik50010@gmail.com>
2023-08-11 16:43:34 +0300

    callback

--- js/script.js
+++ js/script.js
@@ -376,29 +376,27 @@
 
 // fib(0);
 
-function first() {
-  //Do something
-  setTimeout(function () {
-    console.log(1);
-  }, 500);
-}
-function second() {
-  console.log(2);
-}
-first();
-second();
+// function first() {
+//   //Do something
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500);
+// }
+// function second() {
+//   console.log(2);
+// }
+// first();
+// second();
 
+// function learnJS(lang, callback)
+// {
+//     console.log(`I Learn : ${lang}`);
+//     callback();
+// }
 
-function learnJS(lang, callback)
-{
-    console.log(`I Learn : ${lang}`);
-    callback(); 
-}
+// function done()
+// {
+//     console.log('I pass this exam');
+// }
 
-function done()
-{
-    console.log('I pass this exam');
-}
-
-    
-learnJS('JavaScript,' done()); 
+// learnJS('JavaScript', done);
